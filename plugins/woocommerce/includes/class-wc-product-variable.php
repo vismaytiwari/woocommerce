4fffe5d106c1152f8761e8cf6fa2ff1a0e32193a
kalessil <kalessil@gmail.com>
2026-07-10 16:17:01 +0200

    Performance: prime variations gallery caches.

--- plugins/woocommerce/includes/class-wc-product-variable.php
+++ plugins/woocommerce/includes/class-wc-product-variable.php
@@ -337,10 +337,10 @@ class WC_Product_Variable extends WC_Product {
 			$attachment_ids = array();
 			foreach ( $variation_ids as $vid ) {
 				$attachment_ids[] = array( (int) get_post_meta( $vid, '_thumbnail_id', true ) );
-				$attachment_ids[] = explode( ',', (string) get_post_meta( $vid, '_product_image_gallery', true ) );
+				$attachment_ids[] = array_map( 'intval', explode( ',', (string) get_post_meta( $vid, '_product_image_gallery', true ) ) );
 			}
-			$attachment_ids = array_unique( array_filter( array_merge( ...$attachment_ids )) );
-			if ( $attachment_ids ) {
+			$attachment_ids = array_unique( array_filter( array_merge( ...$attachment_ids ) ) );
+			if ( ! empty( $attachment_ids ) ) {
 				_prime_post_caches( $attachment_ids );
 			}
 		}
