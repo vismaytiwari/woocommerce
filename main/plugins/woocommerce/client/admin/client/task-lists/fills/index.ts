79ca172110cecb285a21c6e0ec0ed1ff06c6bbb6
louwie17 <louwie17@users.noreply.github.com>
2026-07-11 03:44:35 +0000

    deploy: 580b090e36514ffc4ee91e63613e6da62b3bfdfd

---
+++ main/plugins/woocommerce/client/admin/client/task-lists/fills/index.ts
@@ -0,0 +1,24 @@
+/**
+ * Internal dependencies
+ */
+import { isImportProduct } from './utils';
+import './PaymentGatewaySuggestions';
+import './shipping';
+import './Marketing';
+import './appearance';
+import './tax';
+import './woocommerce-payments';
+import './deprecated-tasks';
+import './launch-your-store';
+
+const possiblyImportProductTask = async () => {
+	if ( isImportProduct() ) {
+		import( './import-products' );
+	} else {
+		import( './products' );
+	}
+};
+
+possiblyImportProductTask();
+
+import( './shipping-recommendation' );
